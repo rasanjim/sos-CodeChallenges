a78a7ccc72ac5e28050e4873574a619d039f6cb3
rasanjim <rasanen.jimmy.e@gmail.com>
2024-04-02 15:39:46 +0300

    CC- Standardavvikelse

--- app/src/main/java/fi/arcada/codechallenge/MainActivity.java
+++ app/src/main/java/fi/arcada/codechallenge/MainActivity.java
@@ -11,11 +11,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     TextView outputText;
     EditText inputText;
 
+    double [] testdata = { 658457, 297132, 244223, 239206, 209551, 195137, 144473, 121543, 120027, 83482, 80454, 77261, 72634, 67971, 67615, 64736, 64180, 52122, 51400, 51241, 51149, 47909, 46880, 45988, 45226, 38959, 37232, 36493, 35497, 34884 };
+    ArrayList<Double> values = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,16 +28,31 @@ public class MainActivity extends AppCompatActivity {
         outputText = findViewById(R.id.outText);
         inputText = findViewById(R.id.inputText);
 
-        outputText.setText("Min app funkar!");
+        // Vi fyller vår ArrayList med värdena från testdata-arrayen
+       for (double value : testdata) {
+           values.add(value);
+       }
 
+       double version = 3.14159;
+       int year = 2024;
+       String author = "Jimmy";
 
+       outputText.setText(String.format(
+               "Appens version %.1f år %d by %s",
+               version,
+               year,
+               author)
+       );
 
     }
 
     public void calculate(View view) {
-        double[] values = { 3.5, 4.4, 9, 1.2, 6.5, 12.1 };
 
-        outputText.setText(String.format("Medelvärde: %.2f", Statistics.calcMean(values)));
+        outputText.setText(String.format("Medelvärde: %.2f\nMedian: %.2f\nStandardavvikelse: %.2f",
+                Statistics.calcMean(values),
+                Statistics.calcMedian(values),
+                Statistics.calcStdev(values)
+                ));
 
     }
 
--- app/src/main/java/fi/arcada/codechallenge/Statistics.java
+++ app/src/main/java/fi/arcada/codechallenge/Statistics.java
@@ -1,13 +1,42 @@
 package fi.arcada.codechallenge;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Statistics {
 
-    public static double calcMean(double[] values) {
+    public static double calcMedian(ArrayList<Double> values) {
+
+        // Vi skapar en kopia av values
+        ArrayList<Double> sorted = new ArrayList<>(values);
+        // Sedan sorterar vi kopian. Annars sorteras ursprungliga datamängden
+        Collections.sort(sorted);
+
+        // Detta funkar, ännu bättre skulle det vara att kolla skilt för jämna datamängder
+        int midIndex = sorted.size()/2;
+
+        // Medianen är det mittersta värdet i vår sorterade datamängd
+        return sorted.get(midIndex);
+
+    }
+
+    public static double calcMean(ArrayList<Double> values) {
         double sum = 0;
-        for (int i = 0; i < values.length; i++) {
-            sum += values[i];
+        for (int i = 0; i < values.size(); i++) {
+            sum += values.get(i);
         }
 
-        return sum / values.length;
+        return sum / values.size();
+    }
+
+    public static double calcStdev(ArrayList<Double> values) {
+        double mean = calcMean(values);
+        double sumDeviation = 0;
+        for (int i = 0; i < values.size(); i++) {
+            sumDeviation +=Math.pow(values.get(i)-mean, 2);
+        }
+
+        return Math.sqrt(sumDeviation / values.size());
+
     }
 }
